da0cbe63fc58fc34d30b5b770899b11bbf1c61ba
euns3c <eunsk31@hanmail.net>
2020-01-29 17:24:02 +0900

    채널 정보로 변경, 변수 설정

--- channel/login.py
+++ channel/login.py
@@ -4,18 +4,22 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome('C:/chromedriver.exe')
-driver.get("https://admin.atomyticketdev.com/login?error=error&successUrl=%2Fnoticeboard")
+url = 'https://admin.ch.atomystg.com/login?error=error&jisa=KR&successUrl=%2Ftemplates%3Fjisa%3DKR'
+driver.get(url)
 
 #name=id
 #name=pwd
 
-driver.find_element_by_name('id').send_keys('baebs')
-driver.find_element_by_name('pwd').send_keys('mz@0803')
+adm_id='baebs'
+adm_pw='mz@0803'
+
+driver.find_element_by_name('id').send_keys(adm_id)
+driver.find_element_by_name('pwd').send_keys(adm_pw)
 driver.find_element_by_name('pwd').send_keys(Keys.ENTER)
 
 #.panel-heading
-driver.find_elements_by_css_selector('.panel-heading')[3].click()
+#driver.find_elements_by_css_selector('.panel-heading')[3].click()
 #.nav-second>li>a
 #XPATH=//*[@id="collapse3"]/ul/li[1]/a
 
-driver.find_element_by_css_selector('//*[@id="collapse3"]/ul/li[1]/a').click()
+#driver.find_element_by_css_selector('//*[@id="collapse3"]/ul/li[1]/a').click()
