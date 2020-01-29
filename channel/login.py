b51201c405c91870f714da449de227a73c49bec4
eunsk <eunsk31@hanmail.net>
2020-01-29 22:08:33 +0900

    집에서.. [OK] 버튼 못 누름 ㅠㅠ

--- channel/login.py
+++ channel/login.py
@@ -2,6 +2,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome('C:/chromedriver.exe')
 url = 'https://admin.ch.atomystg.com/login?error=error&jisa=KR&successUrl=%2Ftemplates%3Fjisa%3DKR'
@@ -13,13 +14,22 @@ driver.get(url)
 adm_id=''
 adm_pw=''
 
+driver.set_window_size(1920, 1080)
 driver.find_element_by_name('id').send_keys(adm_id)
 driver.find_element_by_name('pwd').send_keys(adm_pw)
 driver.find_element_by_name('pwd').send_keys(Keys.ENTER)
 
-#.panel-heading
-#driver.find_elements_by_css_selector('.panel-heading')[3].click()
-#.nav-second>li>a
-#XPATH=//*[@id="collapse3"]/ul/li[1]/a
+driver.find_element(By.LINK_TEXT, "메뉴관리").click()
 
-#driver.find_element_by_css_selector('//*[@id="collapse3"]/ul/li[1]/a').click()
+# (1) 애터미 소식
+driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) .switch").click()
+driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) .btn-primary").click()
+# driver.implicitly_wait(3000)
+
+# [Front] 애터미 소식 ON/OFF 확인
+#driver.get('https://ch.atomystg.com/kr/notices')
+
+
+# [취소]
+# driver.find_element(By.CSS_SELECTOR, "tr:nth-child(5) .switch").click()
+# driver.find_element(By.CSS_SELECTOR, "tr:nth-child(5) .btn-default").click()
